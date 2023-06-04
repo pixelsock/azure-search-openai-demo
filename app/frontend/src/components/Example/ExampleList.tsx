04eaec151830da965d139e05583402f29782507f
pixelsock <nick@pixelsock.com>
2023-06-04 10:48:57 -0400

    removed unused pdfs

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,14 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "What is the Charlotte Unified Development Ordinance?",
+        value: "What is the Charlotte Unified Development Ordinance?"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    {
+        text: "List the different zoning districts and compaire the major differences between them.",
+        value: "List the different zoning districts and compaire the major differences between them."
+    },
+    { text: "Write a poem explaining the UDO.", value: "Write a poem explaining the UDO." }
 ];
 
 interface Props {
